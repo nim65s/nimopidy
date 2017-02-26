1fe777c8b3f63e8a3bada49c6b0bc2a2d23d28e8
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-02-27 00:10:59 +0100

    mute button, progress' label & wheel

--- src/mopy.jsx
+++ src/mopy.jsx
@@ -50,17 +50,18 @@ class Mopy extends React.Component {
       track: false,
       length: 100,
       now: 0,
-      maxVol: 100,
       volume: 100,
       mute: false,
+      muteIcon: "volume-up",
     }
     this.updateTimePosition = this.updateTimePosition.bind(this)
     this.updateCurrentTrack = this.updateCurrentTrack.bind(this)
     this.updateVolume = this.updateVolume.bind(this)
     this.updateMute = this.updateMute.bind(this)
     this.controlHandlers = this.controlHandlers.bind(this)
-    this.handleSeek = this.handleSeek.bind(this)
-    this.handleVolume = this.handleVolume.bind(this)
+    this.onSeek = this.onSeek.bind(this)
+    this.onVolume = this.onVolume.bind(this)
+    this.onMute = this.onMute.bind(this)
   }
 
   componentDidMount() {
@@ -68,7 +69,7 @@ class Mopy extends React.Component {
       webSocketUrl: 'ws://' + this.props.url + ':6680/mopidy/ws/',
       callingConvention: 'by-position-or-by-name'
     });
-    this.mopidy.on("state:online", () => {this.setState({connected:true}); this.requestCurrentTrack();});
+    this.mopidy.on("state:online", () => {this.setState({connected:true}); this.requestAll();});
     this.mopidy.on("state:offline", () => {this.setState({connected:false});});
     this.timerIDtp = setInterval(() => {if (this.state.connected) {this.requestTimePosition();}}, 100);
     this.timerIDct = setInterval(() => {if (this.state.connected) {this.requestCurrentTrack();}}, 1000);
@@ -76,6 +77,7 @@ class Mopy extends React.Component {
   }
 
 
+  requestAll() { this.requestCurrentTrack(); this.requestTimePosition(); this.requestVolume(); }
   requestTimePosition() { this.mopidy.playback.getTimePosition().done(this.updateTimePosition); }
   requestCurrentTrack() { this.mopidy.playback.getCurrentTrack().done(this.updateCurrentTrack); }
   requestVolume() {
@@ -83,13 +85,14 @@ class Mopy extends React.Component {
     this.mopidy.mixer.getMute().done(this.updateMute);
   }
 
-  updateTimePosition(result) { this.setState({now: result}); }
+  updateTimePosition(time) { this.setState({now: time, nowstr: new Date(time).toISOString().substr(14,5)}); }
   updateCurrentTrack(track) { if (track) { this.setState({track: track, length: track.length}); } }
   updateVolume(vol) { this.setState({volume: vol}); }
-  updateMute(mute) { this.setState({mute: mute}); }
+  updateMute(mute) { this.setState({mute: mute, muteIcon: mute ? "volume-off" : "volume-up"}); }
 
-  handleSeek(time) { this.mopidy.playback.seek({'time_position': time}); }
-  handleVolume(vol) { console.log(vol); this.mopidy.mixer.setVolume({'volume': vol}); this.updateVolume(vol); }
+  onSeek(time) { this.setState({now: time}); this.mopidy.playback.seek({'time_position': time}); }
+  onVolume(vol) { this.mopidy.mixer.setVolume({'volume': vol}); this.updateVolume(vol); }
+  onMute() { this.mopidy.mixer.setMute({"mute": !this.state.mute}); this.updateMute(!this.state.mute); }
 
   componentWillUnmount() {
     this.mopidy.close();
@@ -107,8 +110,10 @@ class Mopy extends React.Component {
       <div>
         <CurrentTrack track={this.state.track} />
         <Controls handlers={this.controlHandlers} />
-        <Progress max={this.state.maxVol} now={this.state.volume} onSeek={this.handleVolume} />
-        <Progress max={this.state.length} now={this.state.now} onSeek={this.handleSeek} />
+        <Button bsSize="large" onClick={this.onMute}><Glyphicon glyph={this.state.muteIcon} /></Button>
+        <Progress onSeek={this.onVolume} max={100} now={this.state.volume} label={this.state.volume} wheelCoef={.1} />
+        <Progress onSeek={this.onSeek} max={this.state.length} now={this.state.now} label={this.state.nowstr}
+          wheelCoef={100} active />
       </div>
       );
   }
--- src/progress.jsx
+++ src/progress.jsx
@@ -5,6 +5,7 @@ class Progress extends React.Component {
   constructor(props) {
     super(props);
     this.click = this.click.bind(this);
+    this.wheel = this.wheel.bind(this);
   }
 
   click(event) {
@@ -13,9 +14,15 @@ class Progress extends React.Component {
     this.props.onSeek(Math.round((event.pageX - rectangle.left) * this.props.max / rectangle.width));
   }
 
+  wheel(e) {
+    this.props.onSeek(Math.round(this.props.now - e.deltaY * this.props.wheelCoef));
+  }
+
   render() {
     return (
-      <ProgressBar active max={this.props.max} now={this.props.now} onClick={this.click} />
+      <ProgressBar onClick={this.click} onWheel={this.wheel}
+        active={this.props.active} max={this.props.max} now={this.props.now} label={this.props.label}
+      />
     );
   }
 }
