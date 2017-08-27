93f4ec4edaf6c5903d7dc1a81236b51d6c5d1ee1
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-08-27 22:20:50 +0200

    as_list

--- musicapp/models.py
+++ musicapp/models.py
@@ -66,8 +66,7 @@ class Playlist(NamedModel):
 
     @classmethod
     def update(cls):
-        # TODO as_list ?
-        for playlist in mopidy_api('core.playlists.get_playlists', include_tracks=False):
+        for playlist in mopidy_api('core.playlists.as_list'):
             Playlist.objects.get_or_create(uri=playlist['uri'], defaults={'name': playlist['name']})
 
     def json(self):
