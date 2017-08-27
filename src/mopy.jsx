31cd8c19a55435b97edd0834a5846b46df48414f
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-08-28 01:40:51 +0200

    conditional cover

--- src/mopy.jsx
+++ src/mopy.jsx
@@ -82,7 +82,7 @@ class Mopy extends React.Component {
     return (
       <div>
         <div className="clearfix">
-          <img src={this.state.track.cover} alt={this.state.track.album} className="pull-left" />
+          {this.state.track.cover ? <img src={this.state.track.cover} alt={this.state.track.album} className="pull-left" /> : ''}
           <h1>{this.state.track.artists}</h1>
           <h2>{this.state.track.name}</h2>
           <h3>{this.state.track.album}</h3>
