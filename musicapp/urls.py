341ae6e8ae581a347766c1b1d7ba746613671657
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-01-29 22:52:04 +0100

    from . import views

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import TrackUpdateView, playlists, snapcast, start_view, webhooks
+from . import views
 
 app_name = 'musicapp'
 urlpatterns = [
-    path('start', start_view, name='webhooks'),
-    path('webhooks', webhooks, name='webhooks'),
-    path('snapcast', snapcast, name='snapcast'),
-    path('playlists', playlists, name='playlists'),
-    path('track/<str:slug>', TrackUpdateView.as_view(), name='track'),
+    path('start', views.start_view, name='webhooks'),
+    path('webhooks', views.webhooks, name='webhooks'),
+    path('snapcast', views.snapcast, name='snapcast'),
+    path('playlists', views.playlists, name='playlists'),
+    path('track/<str:slug>', views.TrackUpdateView.as_view(), name='track'),
 ]
