90ba29e6ae5d287300b78bbfc322f5da28ddb60f
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-12 23:30:51 +0100

    use ndh utils.get_env

--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@
 import os
 import sys
 
+from ndh.utils import get_env
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nimopidy.settings")
+    get_env()
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
